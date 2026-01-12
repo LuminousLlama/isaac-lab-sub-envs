0bcd917163c64102a57b4b2ee5f1b53c08359cd8
LuminousLlama <keyushattarde@gmail.com>
2026-01-12 00:08:43 -0800

    small cleanup

--- source/sub_envs2/sub_envs2/tasks/direct/sub_envs2_marl/sub_envs2_marl_env.py
+++ source/sub_envs2/sub_envs2/tasks/direct/sub_envs2_marl/sub_envs2_marl_env.py
@@ -21,6 +21,8 @@ from isaaclab.assets import RigidObject
 
 from sub_envs2 import subscene
 
+from isaaclab.envs.common import ActionType, AgentID, EnvStepReturn, ObsType, StateType
+
 
 class SubEnvs2MarlEnv(DirectMARLEnv):
     cfg: SubEnvs2MarlEnvCfg
@@ -42,7 +44,6 @@ class SubEnvs2MarlEnv(DirectMARLEnv):
 
         super().__init__(cfg, render_mode, **kwargs)
 
-        self.temp_reset_count = 0
         self.sub_scene_episode_length_buf = torch.zeros(
             self.num_sub_envs, self.num_envs, device=self.device
         )
@@ -71,7 +72,7 @@ class SubEnvs2MarlEnv(DirectMARLEnv):
         light_cfg.func("/World/Light", light_cfg)
 
         # setup sub scenes
-
+        # maybe move inside subscene
         self.sub_scene_0._rigid_objects["cube_red"] = self.cube_red
         self.sub_scene_1._rigid_objects["cube_green"] = self.cube_green
         self.sub_scene_2._rigid_objects["cube_blue"] = self.cube_blue
@@ -82,9 +83,9 @@ class SubEnvs2MarlEnv(DirectMARLEnv):
     def _apply_action(self) -> None:
         action_scale = 1.0
 
+        # TODO maybe move handling apply action inside the sub scene as well?
         current_vel_red = self.cube_red.data.root_link_vel_w
         current_vel_red[:, 2] = self.actions["cube_red"][:, 0] * action_scale
-        # note: this can be applied to only specific envs if needed
         self.cube_red.write_root_link_velocity_to_sim(current_vel_red)
 
         current_vel_green = self.cube_green.data.root_link_vel_w
@@ -96,6 +97,8 @@ class SubEnvs2MarlEnv(DirectMARLEnv):
         self.cube_blue.write_root_link_velocity_to_sim(current_vel_blue)
 
     def _get_observations(self) -> dict[str, torch.Tensor]:
+
+        # TODO maybe move inside sub scene
         obs = {
             "cube_red": self.cube_red.data.root_pos_w,
             "cube_green": self.cube_green.data.root_pos_w,
@@ -105,6 +108,8 @@ class SubEnvs2MarlEnv(DirectMARLEnv):
         return obs
 
     def _get_rewards(self) -> dict[str, torch.Tensor]:
+
+        # TODO maybe move inside sub scene?
         reward = {
             "cube_red": torch.zeros(self.num_envs, device=self.device),
             "cube_green": torch.zeros(self.num_envs, device=self.device),
@@ -114,10 +119,13 @@ class SubEnvs2MarlEnv(DirectMARLEnv):
         return reward
 
     def _get_dones(self) -> tuple[dict[str, torch.Tensor], dict[str, torch.Tensor]]:
+
+        # TODO maybe move inside sub scenes?
         terminated = {
-            "cube_red": torch.zeros(
-                self.num_envs, dtype=torch.bool, device=self.device
-            ),
+            # "cube_red": torch.zeros(
+            #     self.num_envs, dtype=torch.bool, device=self.device
+            # ),
+            "cube_red": torch.rand(self.num_envs, device=self.device) < 0.01,
             "cube_green": torch.zeros(
                 self.num_envs, dtype=torch.bool, device=self.device
             ),
@@ -126,50 +134,56 @@ class SubEnvs2MarlEnv(DirectMARLEnv):
             ),
         }
 
-        # TODO this needs to change for sub envs
         truncated = {
-            "cube_red": self.sub_scene_episode_length_buf[0] >= self.max_episode_length,
+            # "cube_red": self.sub_scene_episode_length_buf[0] >= self.max_episode_length,
+            "cube_red": torch.zeros(
+                self.num_envs, dtype=torch.bool, device=self.device
+            ),
             "cube_green": self.sub_scene_episode_length_buf[1]
             >= self.max_episode_length / 2,
             "cube_blue": self.sub_scene_episode_length_buf[2]
-            >= self.max_episode_length / 4,
+            >= self.max_episode_length / 3,
         }
 
         return terminated, truncated
 
     def _reset_idx(self, env_ids: Sequence[int] | None):
-        # print("PASSED SEQUENCE", env_ids)
+        raise Exception("should never call this function")
 
-        test_envs = torch.tensor([0, 1], device=self.device)
+    def reset(
+        self, seed: int | None = None, options: dict[str, Any] | None = None
+    ) -> tuple[dict[AgentID, ObsType], dict[AgentID, dict]]:
+        """Resets all the environments and returns observations.
 
-        print("in _reset_idx")
-        self.sub_scene_0._reset(test_envs)
+        Args:
+            seed: The seed to use for randomization. Defaults to None, in which case the seed is not set.
+            options: Additional information to specify how the environment is reset. Defaults to None.
 
-        self.temp_reset_count = 0
+                Note:
+                    This argument is used for compatibility with Gymnasium environment definition.
 
-        # TODO reset noise models
-        # TODO reset events
-        # TODO episode length buf
+        Returns:
+            A tuple containing the observations and extras (keyed by the agent ID).
+        """
+        # set the seed
+        if seed is not None:
+            self.seed(seed)
 
-        # super()._reset_idx(env_ids)
+        # reset state of scene
+        indices = torch.arange(self.num_envs, dtype=torch.int64, device=self.device)
+        # !!! This has been modified to be sub scene aware
+        for subscene in self.sub_scenes_dict.values():
+            subscene: subscene.SubScene
+            subscene._reset(indices)
 
-        # TODO this needs to change for sub envs
-        # default_red_state = self.cube_red.data.default_root_state[env_ids].clone()
-        # default_red_state[:, :3] += self.scene.env_origins[env_ids]
+        # update observations and the list of current agents (sorted as in possible_agents)
+        self.obs_dict = self._get_observations()
+        self.agents = [
+            agent for agent in self.possible_agents if agent in self.obs_dict
+        ]
 
-        # default_green_state = self.cube_green.data.default_root_state[env_ids].clone()
-        # default_green_state[:, :3] += self.scene.env_origins[env_ids]
-
-        # default_blue_state = self.cube_blue.data.default_root_state[env_ids].clone()
-        # default_blue_state[:, :3] += self.scene.env_origins[env_ids]
-
-        # self.cube_red.write_root_pose_to_sim(default_red_state[:, :7], env_ids)
-        # self.cube_green.write_root_pose_to_sim(default_green_state[:, :7], env_ids)
-        # self.cube_blue.write_root_pose_to_sim(default_blue_state[:, :7], env_ids)
-
-        # self.cube_red.write_root_velocity_to_sim(default_red_state[:, 7:], env_ids)
-        # self.cube_green.write_root_velocity_to_sim(default_green_state[:, 7:], env_ids)
-        # self.cube_blue.write_root_velocity_to_sim(default_blue_state[:, 7:], env_ids)
+        # return observations
+        return self.obs_dict, self.extras
 
     def step(self, actions: dict[AgentID, ActionType]) -> EnvStepReturn:
         """Execute one time-step of the environment's dynamics.
@@ -236,23 +250,11 @@ class SubEnvs2MarlEnv(DirectMARLEnv):
         self.episode_length_buf += 1  # step in current episode (per env)
         self.common_step_counter += 1  # total step (common for all envs)
 
-        self.sub_scene_episode_length_buf[:] += 1
-
         self.terminated_dict, self.time_out_dict = self._get_dones()
-        self.reset_buf[:] = math.prod(self.terminated_dict.values()) | math.prod(
-            self.time_out_dict.values()
-        )
         self.reward_dict = self._get_rewards()
 
-        # TODO compute the reset_buf with subscene awareness
-        # TODO probably make it so calling _reset_idx() crashes, resets should be handled properly with subscene reset
-        # TODO probably have episode length buf of shape (sub scenes, num_envs)
-        # so you can do sub_scene_epi_buf[subscene_idx, env_idx] = 0 when resetting and then you can also handle truncation gracefully
-        # # -- reset envs that terminated/timed-out and log the episode information
-        # reset_env_ids = self.reset_buf.nonzero(as_tuple=False).squeeze(-1)
-        # if len(reset_env_ids) > 0:
-        #     self._reset_idx(reset_env_ids)
-
+        #  !!! this code has changed to be sub scene aware
+        self.sub_scene_episode_length_buf[:] += 1
         self.reset_sub_scenes()
 
         # post-step: step interval event
@@ -283,35 +285,16 @@ class SubEnvs2MarlEnv(DirectMARLEnv):
         )
 
     def reset_sub_scenes(self):
-
         # loop through all sub scenes
         for key in self.sub_scenes_dict.keys():
             reset_buf = self.terminated_dict[key] | self.time_out_dict[key]
             reset_env_ids = reset_buf.nonzero(as_tuple=False).squeeze(-1)
             self.sub_scenes_dict[key]._reset(reset_env_ids)
 
-        # self.temp_reset_count += 1
-
-        # if self.temp_reset_count > 300:
-        #     idxs = torch.tensor([0], device=self.device)
-        #     self.temp_reset_count = 0
-        #     self.sub_scene_0._reset(idxs)
-
-        # self.terminated_dict, self.time_out_dict = self._get_dones()
-        # self.reset_buf[:] = math.prod(self.terminated_dict.values()) | math.prod(self.time_out_dict.values())
-        # self.reward_dict = self._get_rewards()
-
-        # # -- reset envs that terminated/timed-out and log the episode information
-        # reset_env_ids = self.reset_buf.nonzero(as_tuple=False).squeeze(-1)
-        # if len(reset_env_ids) > 0:
-        #     self._reset_idx(reset_env_ids)
-
 
 # ---------
 # sub scenes
 # ---------
-
-
 class SubScene0(subscene.SubScene):
 
     def _reset(self, env_ids):
